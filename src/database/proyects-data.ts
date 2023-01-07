4d35e8f0e11cdfb7cf5e203d0a89ab8571f5fe68
Armando Murillo <totovip1464@gmail.com>
2023-01-07 13:57:02 -0600

    Agrego nuevos proyectos al Portfolio

--- src/database/proyects-data.ts
+++ src/database/proyects-data.ts
@@ -10,7 +10,7 @@ export interface seedProyects {
     technologys: ValidTechnologys[],
 }
 
-type ValidTechnologys = 'JavaScript'|'NodeJS'|'React'|'NextJS'|'CSS'|'Sass'|'MongoDB'|'HTML'|'TypeScript';
+type ValidTechnologys = 'JavaScript'|'NodeJS'|'React'|'NextJS'|'CSS'|'Sass'|'MongoDB'|'HTML'|'TypeScript'|'MaterialUI'|'NextUI';
 
 interface SeedData {
     proyects: seedProyects[]
@@ -25,7 +25,7 @@ export const proyectsData: SeedData = {
             image: 'https://i.ibb.co/JWZqCNm/MovieApp.png',
             link: 'https://movie-app-2022-cm.vercel.app/',
             github: 'https://github.com/Armandocm19/movie-app',
-            technologys: ['React', 'NextJS', 'TypeScript', 'CSS', 'HTML'],
+            technologys: ['NextJS', 'TypeScript', 'CSS', 'HTML', 'NextUI'],
         },
         {
             title: 'PokemonAppStatic',
@@ -33,15 +33,31 @@ export const proyectsData: SeedData = {
             image: 'https://i.ibb.co/P6NXnj0/PokeApp.png',
             link: 'https://pokemonappstatic.vercel.app/',
             github: 'https://github.com/Armandocm19/PokemoApi',
-            technologys: ['React', 'NextJS', 'TypeScript', 'CSS', 'HTML']
+            technologys: ['NextJS', 'TypeScript', 'CSS', 'HTML']
         },
         {
             title: 'Galería Web',
-            description: 'Web donde muestra los Parques Nacionales de unos de mis países favoritos',
+            description: 'Web donde muestra los Parques Nacionales de unos de mis países favoritos.',
             image: 'https://i.ibb.co/b2vQW7j/Galeria-Web.png',
             link: 'https://galeria-web.vercel.app/',
             github: 'https://github.com/Armandocm19/GaleriaWeb',
             technologys: ['JavaScript', 'CSS', 'HTML']
+        },
+        {
+            title: 'Teslo-Shop (Clon)',
+            description: 'Esta es un clon de la tienda Teslo-Shop, la cuál contiene su propia base de datos, y área administrativa.',
+            image: 'https://i.ibb.co/2nsTrGW/Teslo-Shop.png',
+            link: 'https://next-teslo-shop-2023.vercel.app/',
+            github: 'https://github.com/Armandocm19/next-teslo-shop',
+            technologys: ['NextJS','TypeScript', 'CSS', 'HTML', 'MongoDB']
+        },
+        {
+            title: 'Helados Artesanales',
+            description: 'Esta es una web para el emprendimiento de un cliente, donde se pueden hacer órdenes,y además permite tener un control del inventario.',
+            image: 'https://i.ibb.co/80WcNRw/Helados-Artesanales.png',
+            link: 'https://helados-artesanales.up.railway.app/',
+            github: 'https://github.com/Armandocm19/helados-artesanales',
+            technologys: ['NextJS', 'TypeScript', 'CSS', 'HTML', 'MongoDB']
         }
     ]
 
