1ca11525410958d69f84243d3b7854f7f722b1f2
Armando Murillo <totovip1464@gmail.com>
2023-02-05 11:17:30 -0600

    Elimino meta tags innecesarios, y agrego algunos

--- src/components/layout/PortfolioLayout.tsx
+++ src/components/layout/PortfolioLayout.tsx
@@ -9,12 +9,6 @@ export const PortfolioLayout: FC<Props> = ({ children  }) => {
 
     return (
         <>
-            <header>
-
-            <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet"></link>  
-
-            </header>
-
             <nav>
                 <NavBar />
             </nav>
