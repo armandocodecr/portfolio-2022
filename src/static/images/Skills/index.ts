3a9d4050cd55110db3f7963f6a486f5cf76be7a0
ArmandoCode <totovip1464@gmail.com>
2023-05-11 23:20:32 -0600

    Elimino tecnologías de la seccion de 'conocimientos'

--- src/static/images/Skills/index.ts
+++ src/static/images/Skills/index.ts
@@ -1,6 +1,4 @@
- import html from './html.png'
  import docker from './docker.png'
- import css from './css.png'
  import js from './js.png'
  import mongo from './mongodb.webp'
  import next from './next.webp'
@@ -8,13 +6,10 @@
  import react from './react.png'
  import sql from './sql.png'
  import typescript from './typescript.png'
- import github from './github.png'
  import git from './git.png'
 
  const SkillsImages = [
-    {link: html, title: 'HTML'},
     {link: docker, title: 'Docker'},
-    {link: css, title: 'CSS'},
     {link: js, title: 'JavaScript'},
     {link: mongo, title: 'MongoDB'},
     {link: next, title: 'NextJS'},
@@ -22,7 +17,6 @@
     {link: react, title: 'React'},
     {link: sql, title: 'SQL'},
     {link: typescript, title: 'TypeScript'},
-    {link: github, title: 'GitHub'},
     {link: git, title: 'Git'},
  ]
 
