9e2ee9d7830c8c979400b472d0fcd4bf2bbf7d83
ArmandoCode <totovip1464@gmail.com>
2023-05-11 23:21:00 -0600

    Footer.tsx: Modifico el titulo del footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -3,25 +3,27 @@ import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import './Styles.css'
 
 export const Footer = () => {
+  
+  const date = new Date().getFullYear();
 
-    return(
-        <>
-            <Text color="secondary" weight='bold' 
-            css={{ textGradient: "45deg, $blue600 -20%, $pink600 50%",}}>
-                Web developer by Armando Cortés Murillo. ©2022. All rights reserved.
-            </Text>
-            <div className="container-icons-footer">
-            <a href='https://github.com/Armandocm19' target='_blank' rel="noreferrer">
-              <FaGithub className='iconos-navbar' id='git' />
-            </a>
-            <a href='https://www.linkedin.com/in/armando-murillo/' target='_blank' rel="noreferrer">
-              <FaLinkedin className='iconos-navbar' id='linkedin' />
-            </a>
-            <a href='https://twitter.com/CodeArmando' target='_blank' rel="noreferrer">
-              <FaTwitter className='iconos-navbar' id='twitter' />
-            </a>
-          </div>
-        </>
-    )
+  return(
+      <>
+          <Text color="secondary" weight='bold' 
+          css={{ textGradient: "45deg, $blue600 -20%, $pink600 50%",}}>
+              Web developer Armando Cortés Murillo.&nbsp;{date}
+          </Text>
+          <div className="container-icons-footer">
+          <a href='https://github.com/Armandocm19' target='_blank' rel="noreferrer">
+            <FaGithub className='iconos-navbar' id='git' />
+          </a>
+          <a href='https://www.linkedin.com/in/armando-murillo/' target='_blank' rel="noreferrer">
+            <FaLinkedin className='iconos-navbar' id='linkedin' />
+          </a>
+          <a href='https://twitter.com/CodeArmando' target='_blank' rel="noreferrer">
+            <FaTwitter className='iconos-navbar' id='twitter' />
+          </a>
+        </div>
+      </>
+  )
 
 }
